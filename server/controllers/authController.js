b89dc9167e8fbcc83da5e66e840abe300a1ca896
Ben Smerd <bsmerd@adam.com.au>
2024-04-16 20:25:21 +0930

    create auth routes and controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,9 @@
+const db = require("../connection/database");
+
+// TODO will need to incorporate JWT for security and probably some other stuff
+
+const authHttp = {
+    login: async (req, res) => {
+        const sql = "";
+    },
+};
